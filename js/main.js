cb185be15fdc0f32ebf02f1fcb4fc9d17424ed93
Budai Dani <budai.d945@gmail.com>
2016-05-15 11:42:48 +0200

    business logic added to main.js in js folder

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function validateContactData(name, email, msg) {
+    var reEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var checkName = /^[a-zA-Z.]{2,30}$/
+    var checkMsg = /^.{20,500}$/
+    return checkName.test(name);
+}
+
+function sendMessage(name, email, msg) {
+    if (validateContactData(name, email, msg)) {
+        alert("Your name, email or message is not correct!");
+    } else {
+        alert("Your message was sent!");
+    }
+
+}
